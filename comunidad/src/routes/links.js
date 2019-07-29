9fd58b1b85807240053f89005bbefd5e95345f31
patoCode <e3gan.root@gmail.com>
2019-07-29 11:54:54 -0400

    commit views config

--- comunidad/src/routes/links.js
+++ comunidad/src/routes/links.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const pool = require('../database')
+
+router.get('/add', (req, res) => {
+  res.render('links/add')
+})
+
+router.post('/add', (req, res) => {
+  res.send('ok!')
+})
 
 module.exports = router
